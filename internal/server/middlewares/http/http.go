3fd84261c659dbb6cbf4476b0d1bae6f2c9298d2
ahsouza <annibalhsouza@gmail.com>
2021-08-11 11:57:57 -0300

    feat: add server

---
+++ internal/server/middlewares/http/http.go
@@ -0,0 +1,24 @@
+package serve
+
+import (
+	"fmt"
+
+	"github.com/soheilhy/args"
+)
+
+var ToListen = args.NewInt(args.Default(4200))
+var BufferSize = args.NewUint64(args.Default(uint64(1024 * 1024)))
+var StateDir = args.NewString(args.Flag("test.state.dir", "/tmp", "state dir"))
+
+//var RoundTripper = args.New(Default(http.DefaultTransport))
+//var Timeout = args.NewDuration(Flag("timeout", 10*time.Second, "timeout"))
+
+func Anibal(opts ...args.V) {
+	port := ToListen.Get(opts)
+	//bufs := BufferSize.Get(opts)
+	//sdir := StateDir.Get(opts)
+	//rt := RoundTripper.Get(args)
+	//to := Timeout.Get(args)
+
+	fmt.Printf("Server listening on the address: http://localhost:%d\n", port)
+}
